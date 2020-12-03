b984b592b296d0e723090cd839617ef65a42fb40
juliano.valente <juliano.valente@gmail.com>
2020-12-03 09:02:26 -0500

    Atualiado

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabeçalho da Página.")
+        self.assertRegex(result.data.decode(), "Aqui fica o cabeçalho da pagina alterada by JOV.")
 
 
 if __name__ == "__main__":
